d102050f4f36cbbedbff52122983e1e481ae6eec
SimonOkello <simonokello93@gmail.com>
2019-10-14 15:09:12 +0300

    Initial Commit

--- meetup/urls.py
+++ meetup/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from meetup import views
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    
 ]
--- meetup/views.py
+++ meetup/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+def register(request):
+    return render(request, 'signup.html', {})
+
